829c9cf63159132152acacfbcc51fbd61bd6f574
katie <kjewett17@gmail.com>
2023-04-10 15:27:28 -0400

    countries component and styles

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -12,8 +12,26 @@ const Countries = () => {
             </section>
 
 
-            <section>
-                Eventually there will be a bunch of countries listed here
+            <section className="countries-container">
+                <div className="country-card">
+                    <img src="" alt="flag" className="country-flag"/>
+                    <div className="country-snapshot">
+                        <h2 className="country-name">Germany</h2>
+                        
+                        <div className="country-stat">
+                            <p className="stat-name">Population:</p>
+                            <p className="stat-value">81,770,900</p>
+                        </div>
+                        <div className="country-stat">
+                            <p className="stat-name">Region:</p>
+                            <p className="stat-value">Europe</p>
+                        </div>
+                        <div className="country-stat">
+                            <p className="stat-name">Capital:</p>
+                            <p className="stat-value">Berlin</p>
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     )
