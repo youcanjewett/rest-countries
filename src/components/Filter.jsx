af0d1e7843350b46acdaa0deee32c6a816750733
katie <kjewett17@gmail.com>
2023-04-10 11:39:27 -0400

    filter styles

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import './../styles/filter.css';
 
 const Filter = () => {
+
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    const openDropdown = () => {
+        let dropdown = document.getElementById("myDropdown");
+        if (dropdown.style.display === 'none')
+            dropdown.style.display = 'block';
+        else {
+            dropdown.style.display = 'none';
+        }
+    }
+    
     return (
-        <div>I'll be a filter when I grow up</div>
+        <div class="dropdown">
+            <button onClick={openDropdown} className="dropbtn">
+                Filter by Region
+                <img src='chevron-down.svg' className="chevron" alt="chevron down"/>
+            </button>
+            <div id="myDropdown" class="dropdown-content">
+                <a href="#africa">Africa</a>
+                <a href="#america">America</a>
+                <a href="#asia">Asia</a>
+                <a href="#europe">Europe</a>
+                <a href="#oceania">Oceania</a>
+            </div>
+        </div>
     )
 }
 
