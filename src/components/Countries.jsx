9bf67b65cbcaaed1178751434aed3bb12e8523da
katie <kjewett17@gmail.com>
2023-04-18 15:19:15 -0400

    filter component
    adding logic to display filtered countries
    refactoring styles

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import { Link } from "react-router-dom";
 import SearchBar from './SearchBar';
 import Filter from './Filter';
 import axios from 'axios';
@@ -9,6 +10,7 @@ const url = 'https://restcountries.com/v2/all'
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
+    const [filteredCountries, setFilteredCountries] = useState([]);
 
     useEffect (() => {
 
@@ -16,7 +18,6 @@ const Countries = () => {
 
             try {
                 const results = await axios.get(url);
-                console.log(results.data);
                 setCountries(results.data);
             } catch (error) {
                 console.log(error);
@@ -32,35 +33,86 @@ const Countries = () => {
 
     return (
         
-        <main className="main-container">
+        < >
             <section className="filter-container">
                 <SearchBar />
-                <Filter />
+                <Filter 
+                countries = {countries}
+                setCountries={setCountries}
+                setFilteredCountries={setFilteredCountries}
+                    />
             </section>
 
+            {filteredCountries.length !== 0 ? (
+                <section className="country-grid">
+                {filteredCountries.map((country) => {
+                    const {numericCode, name, flag, population, region, capital} = country
+                    
+                return (
+                <Link to={`/${name}`} key={numericCode} >
+                    <article>
+                        <div className="country-card">
 
-            <section className="countries-container">
-                <div className="country-card">
-                    <img src="" alt="flag" className="country-flag"/>
-                    <div className="country-snapshot">
-                        <h2 className="country-name">{countries.name}</h2>
-                        
-                        <div className="country-stat">
-                            <p className="stat-name">Population:</p>
-                            <p className="stat-value">81,770,900</p>
+                        <img src={flag} alt={name} />
+                        <div className="country-snapshot">
+                            <h2 className="country-name">{name}</h2>
+                            
+                            <div className="country-stat">
+                                <p className="stat-name">Population:</p>
+                                <p className="stat-value">{population}</p>
+                            </div>
+                            <div className="country-stat">
+                                <p className="stat-name">Region:</p>
+                                <p className="stat-value">{region}</p>
+                            </div>
+                            <div className="country-stat">
+                                <p className="stat-name">Capital:</p>
+                                <p className="stat-value">{capital}</p>
+                            </div>
                         </div>
-                        <div className="country-stat">
-                            <p className="stat-name">Region:</p>
-                            <p className="stat-value">Europe</p>
                         </div>
-                        <div className="country-stat">
-                            <p className="stat-name">Capital:</p>
-                            <p className="stat-value">Berlin</p>
-                        </div>
-                    </div>
-                </div>
+                    </article>
+                </Link>
+                )
+                })}
             </section>
-        </main>
+            ) : (
+
+
+            <section className="country-grid">
+                {countries.map((country) => {
+                    const {numericCode, name, flag, population, region, capital} = country
+                    
+                return (
+                <Link to={`/${name}`} key={numericCode} >
+                    <article>
+                        <div className="country-card">
+
+                        <img src={flag} alt={name} />
+                        <div className="country-snapshot">
+                            <h2 className="country-name">{name}</h2>
+                            
+                            <div className="country-stat">
+                                <p className="stat-name">Population:</p>
+                                <p className="stat-value">{population}</p>
+                            </div>
+                            <div className="country-stat">
+                                <p className="stat-name">Region:</p>
+                                <p className="stat-value">{region}</p>
+                            </div>
+                            <div className="country-stat">
+                                <p className="stat-name">Capital:</p>
+                                <p className="stat-value">{capital}</p>
+                            </div>
+                        </div>
+                        </div>
+                    </article>
+                </Link>
+                )
+                })}
+            </section>
+            )}
+        </>
     )
 }
 
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,34 +1,50 @@
-import React from 'react';
-import './../styles/filter.css';
 
-const Filter = () => {
+import React, { useEffect } from "react";
+import "./../styles/filter.css";
 
-    /* When the user clicks on the button,
+const Filter = ({ countries, setCountries, setFilteredCountries }) => {
+
+  /* When the user clicks on the button,
     toggle between hiding and showing the dropdown content */
-    const openDropdown = () => {
-        let dropdown = document.getElementById("myDropdown");
-        if (dropdown.style.display === 'none')
-            dropdown.style.display = 'block';
-        else {
-            dropdown.style.display = 'none';
-        }
+  const toggleDropdown = () => {
+    let dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "none") dropdown.style.display = "block";
+    else {
+      dropdown.style.display = "none";
     }
-    
-    return (
-        <div class="dropdown">
-            <button onClick={openDropdown} className="dropbtn">
-                Filter by Region
-                <img src='chevron-down.svg' className="chevron" alt="chevron down"/>
-            </button>
-            <div id="myDropdown" class="dropdown-content">
-                <a href="#africa">Africa</a>
-                <a href="#america">America</a>
-                <a href="#asia">Asia</a>
-                <a href="#europe">Europe</a>
-                <a href="#oceania">Oceania</a>
-            </div>
-        </div>
-    )
+  };
+
+  const filterRegions = (region) => {
+    const filteredCountries = countries.filter(
+      (country) => country.region.toLowerCase() === region.toLowerCase()
+    );
+    setFilteredCountries(filteredCountries);
+
+    toggleDropdown();
+  };
+
+  useEffect(() => {
+   
+    filterRegions();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="dropdown">
+    <button onClick={toggleDropdown} className="dropbtn">
+        Filter by Region
+        <img src='chevron-down.svg' className="chevron" alt="chevron down"/>
+    </button>
+    <div id="myDropdown" className="dropdown-content">
+        <a href="#africa">Africa</a>
+        <a href="#america">America</a>
+        <a href="#asia">Asia</a>
+        <a href="#europe">Europe</a>
+        <a href="#oceania">Oceania</a>
+    </div>
+</div>
+
+)
 }
 
 export default Filter;
